41b047e5eaea201417ba16acaecdd2e7fef3b647
mathias bigler <orchidevej@hotmail.com>
2018-09-26 10:32:58 +0100

    hello my friend

--- tester.py
+++ tester.py
@@ -2,23 +2,35 @@ import collections
 import numpy as np
 import utility
 
+reader = utility.readFile() # data
 
 # 1 Which state has the most deaths in the year of 2016? (All causes)
 
 def state_with_most_deaths_in_2016():
-    reader = utility.readFile()
     state = ""
-    currentMostDeaths = 0
+    current_most_deaths = 0
     for line in reader:
-        if line[3] != 'United States' and line[0] == '2016' and line[2] == 'All causes' and int(line[4]) > currentMostDeaths:
-            currentMostDeaths = int(line[4])
+        if line[3] != 'United States' and line[0] == '2016' and line[2] == 'All causes' and int(line[4]) > current_most_deaths:
+            current_most_deaths = int(line[4])
             state = line[3] 
-    print(state, currentMostDeaths)
+    print(state, current_most_deaths)
     
-state_with_most_deaths_in_2016()
+ # state_with_most_deaths_in_2016()
+
+ # 1 Which state has the least deaths in the year of 2016? (All causes)
+
+def state_with_least_deaths_in_2016():
+    
+    state = ""
+    current_least_deaths = 100000000000
+    for line in reader:
+        if line[3] != 'United States' and line[0] == '2016' and line[2] == 'All causes' and int(line[4]) < current_least_deaths:
+            current_least_deaths = int(line[4])
+            state = line[3] 
+    print(state, current_least_deaths)
 
   
-
+# state_with_least_deaths_in_2016()
 
 
 
